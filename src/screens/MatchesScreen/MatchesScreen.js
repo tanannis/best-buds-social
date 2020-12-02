b565119a8f4580b7b1fef61c2b69813235264e82
Yael Bassal <ybassal@hotmail.com>
2020-12-02 15:30:44 -0500

    MatchesScreen and style.js updated with working Search Bar

--- src/screens/MatchesScreen/MatchesScreen.js
+++ src/screens/MatchesScreen/MatchesScreen.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
 import { SearchBar } from 'react-native-elements'
-// import { firebase } from '../../firebase/config'
-
+import styles from "./styles";
 
 export default function MatchesScreen({ navigation }) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const onChangeSearch = query => setSearchTerm(query);
+
     return (
         <View >
+            <SearchBar
+            style={styles.searchbar}
+            placeholder="Type Here to Search..."
+            onChangeText={onChangeSearch}
+            value={searchTerm}
+            />
         </View>
     )
 }
+
+//Search Bar Documentation: https://callstack.github.io/react-native-paper/searchbar.html
+//Search Bar must be updated when data is in this component.
