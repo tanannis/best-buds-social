b4b487061e78972cd76397b312f3ebd5219a8468
Yael Bassal <ybassal@hotmail.com>
2020-12-02 18:01:14 -0500

    in process - working on chat feature

--- src/screens/MatchesScreen/MatchesScreen.js
+++ src/screens/MatchesScreen/MatchesScreen.js
@@ -2,22 +2,68 @@ import React, { useState } from 'react'
 import { View } from 'react-native'
 import { SearchBar } from 'react-native-elements'
 import styles from "./styles";
+//Newly Added
+import { createStackNavigator } from '@react-navigation/stack';
+import { IconButton } from 'react-native-paper';
+import { SingleChatRoom } from "../SingleChatRoom"
 
-export default function MatchesScreen({ navigation }) {
-    const [searchTerm, setSearchTerm] = useState("");
-    const onChangeSearch = query => setSearchTerm(query);
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
 
-    return (
-        <View >
-            <SearchBar
-            style={styles.searchbar}
-            placeholder="Type Here to Search..."
-            onChangeText={onChangeSearch}
-            value={searchTerm}
+export default function MatchesScreen() {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee'
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22
+        }
+      }}
+    >
+      <ChatAppStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <IconButton
+              icon="message-plus"
+              size={28}
+              color="#ffffff"
+              onPress={() => navigation.navigate('AddRoom')}
             />
-        </View>
-    )
+          )
+        })}
+      />
+      {/* Add this */}
+      <ChatAppStack.Screen name="Room" component={RoomScreen} />
+    </ChatAppStack.Navigator>
+  );
 }
+
+// rest of the code remains same
+
+    //searchbar code below
+    // const [searchTerm, setSearchTerm] = useState("");
+    // const onChangeSearch = query => setSearchTerm(query);
+
+    // return (
+        //Search bar code below
+        // <View >
+        //     <SearchBar
+        //     style={styles.searchbar}
+        //     placeholder="Type Here to Search..."
+        //     onChangeText={onChangeSearch}
+        //     value={searchTerm}
+        //     />
+        // </View>
+    // )
+// }
 
 //Search Bar Documentation: https://callstack.github.io/react-native-paper/searchbar.html
 //Search Bar must be updated when data is in this component.
+
+//Chat Room Documentation: https://amanhimself.dev/blog/chat-app-with-react-native-part-3
+//https://edisondevadoss.medium.com/react-native-chat-using-firebase-d4c0ef1ab0b5
