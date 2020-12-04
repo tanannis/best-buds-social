23e09ba7486e1d1527cdc3642f52a0112862912a
tanannis <tanannis@gmail.com>
2020-12-04 14:41:04 -0500

    added something

---
+++ src/screens/SingleChatRoom/SingleChatRoom.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+
+export default function SingleChatRoom() {
+	return (
+		<View>
+			<Text>Single Chat Room</Text>
+		</View>
+	);
+}
