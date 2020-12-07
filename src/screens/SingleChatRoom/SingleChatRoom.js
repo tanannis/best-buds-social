5f1adf9f97a9044dc87ec43e99cc505bd29b36bf
Madison Kneller <knellermadison@gmail.com>
2020-12-07 16:43:40 -0600

    connected image to firebasestorage

--- src/screens/SingleChatRoom/SingleChatRoom.js
+++ src/screens/SingleChatRoom/SingleChatRoom.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
-import { GiftedChat } from 'react-native-gifted-chat';
+import React, { useEffect, useState } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import { firebase } from "../../firebase/config";
 
 export default function SingleChatRoom() {
-    const [messages, setMessages] = useState([
-       
-  
-    ]);
+  const [messages, setMessages] = useState([]);
 
-// helper method that sends a message in a particular chatroom
-//The newMessage is concatenated with previous or the initial messages using GiftedChat.append() method.
+  useEffect(() => {
+    const chatroomRef = firebase
+      .firestore()
+      .collection("ChatRooms")
+      .doc("UvBpiiWrMT0qJo5nsXhQ");
+    const doc = chatroomRef.get();
+    if (!doc.exists) {
+      console.log("No such document!");
+    } else {
+      console.log("Document data:", doc.data());
+    }
+  });
+
+  // helper method that sends a message in a particular chatroom
+  //The newMessage is concatenated with previous or the initial messages using GiftedChat.append() method.
   function handleSend(newMessage = []) {
     setMessages(GiftedChat.append(messages, newMessage));
   }
@@ -16,7 +27,7 @@ export default function SingleChatRoom() {
   return (
     <GiftedChat
       messages={messages}
-      onSend={newMessage => handleSend(newMessage)}
+      onSend={(newMessage) => handleSend(newMessage)}
       user={{ _id: 1 }}
     />
   );
