162926506e9f57fbee3559ec3d8465cf366d4f54
tanannis <tanannis@gmail.com>
2020-12-08 11:54:38 -0500

    Shows user location by zipcode, but need to fix location mapping

--- src/screens/SetLocationScreen/SetLocationScreen.js
+++ src/screens/SetLocationScreen/SetLocationScreen.js
@@ -1,48 +1,53 @@
 /* function to set your location here */
-
-//the map will show here when enter address to find the location
-
-//maybe show some parks here on the map?
-
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text } from "react-native";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 
 export default function SetLocationScreen() {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null)
+	const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [place, setPlace] = useState(null)
+    const [zipCode, setZipCode] = useState(null)
 
-    useEffect(() => {
-        (async () => {
-          let { status } = await Location.requestPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-          }
-    
-          let location = await Location.getCurrentPositionAsync({});
-          setLocation(location);
-        })();
-      }, []);
-    
-      let text = 'Waiting..';
-      if (errorMsg) {
-        text = errorMsg;
-      } else if (location) {
-        text = JSON.stringify(location);
-      }
-      console.log('MY LOCATION', location)
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestPermissionsAsync();
+			if (status !== "granted") {
+				setErrorMsg("Permission to access location was denied");
+			}
 
-    
-      return (
-        <View style={styles.container}>
-          <Text>{text}</Text>
-        </View>
-      );
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-      },
-    });
+			let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            
+            let place = await Location.reverseGeocodeAsync({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+            }); setPlace(place);
+
+            place.find(place=>{
+                setZipCode(place.postalCode)
+            })
+		})();
+	}, []);
+
+   
+	let text = "Loading location info...";
+	if (errorMsg) {
+		text = errorMsg;
+	} else if (location) {
+		text = JSON.stringify(location);
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Location: {zipCode}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
