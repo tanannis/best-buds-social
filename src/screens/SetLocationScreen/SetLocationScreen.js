fe971289455af311a167662d7704968f4a532411
Yael Bassal <ybassal@hotmail.com>
2020-12-12 15:00:37 -0500

    FilterComponent zipcode field adding to user doc from SetLocationScreen

--- src/screens/SetLocationScreen/SetLocationScreen.js
+++ src/screens/SetLocationScreen/SetLocationScreen.js
@@ -4,57 +4,66 @@ import { View, StyleSheet } from "react-native";
 import { Text } from "react-native";
 import * as Location from "expo-location";
 import { FontAwesome } from "@expo/vector-icons";
+import { firebase } from "../../firebase/config";
 
 export default function SetLocationScreen() {
-	const [location, setLocation] = useState(null);
-	const [errorMsg, setErrorMsg] = useState(null);
-	const [place, setPlace] = useState(null);
-	const [zipCode, setZipCode] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [place, setPlace] = useState(null);
+  const [zipCode, setZipCode] = useState(null);
+  const currentUser = firebase.auth().currentUser;
 
-	useEffect(() => {
-		(async () => {
-			let { status } = await Location.requestPermissionsAsync();
-			if (status !== "granted") {
-				setErrorMsg("Permission to access location was denied");
-			}
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
 
-			let location = await Location.getCurrentPositionAsync({});
-			setLocation(location);
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
 
-			let place = await Location.reverseGeocodeAsync({
-				latitude: location.coords.latitude,
-				longitude: location.coords.longitude,
-			});
-			setPlace(place);
+      let place = await Location.reverseGeocodeAsync({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      setPlace(place);
 
-			place.find((place) => {
-				setZipCode(place.postalCode);
-			});
-		})();
-	}, []);
+      place.find((place) => {
+        setZipCode(place.postalCode);
+      });
 
-	console.log("LOCATION", location);
-	console.log("PLACE", place);
-	console.log("ZIPCODE", zipCode);
+      //firebase query to add location field
+      await firebase
+        .firestore()
+        .collection("users")
+        .doc(currentUser.uid)
+        .set({ location: place.postalCode }, { merge: true });
+    })();
+  }, []);
 
-	let text = "Loading location info...";
-	if (errorMsg) {
-		text = errorMsg;
-	} else if (location) {
-		text = JSON.stringify(location);
-	}
+  console.log("LOCATION", location);
+  console.log("PLACE", place);
+  console.log("ZIPCODE", zipCode);
 
-	return (
-		<View style={styles.container}>
-			<Text>
-				<FontAwesome name="map-marker" size={20} color="blue" /> {zipCode}
-			</Text>
-		</View>
-	);
+  let text = "Loading location info...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        <FontAwesome name="map-marker" size={20} color="blue" /> {zipCode}
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
+  container: {
+    flex: 1,
+  },
 });
